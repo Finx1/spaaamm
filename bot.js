d15a2a70978401c9fabee0ed57edb49c02e86554
Finx1 <49606811+Finx1@users.noreply.github.com>
2020-04-12 01:44:05 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,329 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : DrTriCk  `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+client.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client2.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client2.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client3.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client3.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client4.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client4.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client5.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client5.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client6.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client6.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client7.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client7.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client8.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client8.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client9.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client10.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client10.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client11.on('ready', async() => {
+var server = "555874796378783754"; // ايدي السررفر
+var channel = "555874987555160132";//ايدي الروم
+    setInterval(()=>{
+    client11.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+
+client.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+//-------------------------------------------------1
+
+client2.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+//-------------------------------------------------2
+client3.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client3.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+//-------------------------------------------------3
+client4.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client4.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+//-------------------------------------------------5
+client5.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client5.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+//-------------------------------------------------
+
+client6.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client6.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+//-------------------------------------------------
+
+client7.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client7.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+   
+//-------------------------------------------------
+client8.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client8.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+ });
+//-------------------------------------------------
+client9.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client9.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+client10.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client10.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+client11.on('message', message => {
+    if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+client11.on('message', message => {
+    if(message.content === '2'){
+        message.channel.send('#credits')
+    }
+});
+
+//-------------------------------------------------
+client.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874868734722061";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+
+client2.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874879887507467";
+    setInterval(()=>{
+    client2.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+
+client3.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874893036781617";
+    setInterval(()=>{
+    client3.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+
+client4.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874903195254790";
+    setInterval(()=>{
+    client4.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+client5.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874922048782346";
+    setInterval(()=>{
+    client5.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+
+client6.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874931133513748";
+    setInterval(()=>{
+    client6.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+client7.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874943884197891";
+    setInterval(()=>{
+    client7.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+client8.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874953178644509";
+    setInterval(()=>{
+    client8.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+client9.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874968106434571";
+    setInterval(()=>{
+    client9.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+client10.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874978076295186";
+    setInterval(()=>{
+    client10.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+
+client11.on('ready', async() => {
+var server = "555874796378783754"; 
+var channel = "555874796378783757";
+    setInterval(()=>{
+    client11.guilds.get(server).channels.get(channel).send('by , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk , DrTriCk')
+    },50);
+})
+
+
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN3);
+client4.login(process.env.TOKEN4);
+client5.login(process.env.TOKEN5);
+client6.login(process.env.TOKEN6);
+client7.login(process.env.TOKEN7);
+client8.login(process.env.TOKEN8);
+client9.login(process.env.TOKEN9);
+client10.login(process.env.TOKEN10);
+client11.login(process.env.TOKEN11);
+
